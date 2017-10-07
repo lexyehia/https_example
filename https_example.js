257544fe0ba0cfeb4565258bd2ac4f7ef5edf352
Chad Yehia <lex.yehia@icloud.com>
2017-10-07 22:46:30 +0000

    Step 3 done

--- https_example.js
+++ https_example.js
@@ -1,3 +1,1 @@
-const https = require('https')
-
-console.log('I did it!')
+const https = require('https')const options = {    host: 'www.example.org',    path: '/'}const callback = () => console.log('In response handler callback!')console.log("I'm about to make the request!")https.request(options, callback).end()console.log("I've made the request!")
